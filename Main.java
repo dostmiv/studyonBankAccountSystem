9e2d33fd881cb378f307042a809411892ec79898
dostmiv <39131023+dostmiv@users.noreply.github.com>
2021-02-21 00:23:05 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main (String [] Args) {
-		Account Account1 = new Account(117884332,-1,"Mert Sondas");
+		Account Account1 = new Account(1266324332,-1,"rakip kubaran");//changed due to privacy
 		Account1.showInf();
 		Account1.withdraw(350);
 		Account1.showInf();
